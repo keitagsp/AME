510ac5aee42c918228408b453d41176420e42455
keitagsp <83049576+keitagsp@users.noreply.github.com>
2021-05-04 19:26:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const change = document.getElementById('btn1');
+const rains = document.getElementById('rains');
+const span = document.querySelectorAll('span');
+
+ change.addEventListener('click', () => {
+    //  document.getElementById('fine').classList.toggle('rains');
+    //  rains.classList.remove(rains);s
+     rains.removeChild();
+ });
